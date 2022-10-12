b674348b808a3e996e5e54bfbff1cb1df79b0171
souhaila harrak <souhaila.harrak.solicode@gmail.com>
2022-10-13 00:18:32 +0200

    add search dataTables

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Crud PhPmvc</title>
     <script src="https://kit.fontawesome.com/c6d970a90e.js" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 </head>
 
 <body>
@@ -36,7 +37,7 @@
             <button type="submit" class="btn btn-primary" name="btnregister" value="ok">Submit</button>
         </form>
         <div class="col-8 p-4">
-            <table class="table">
+            <table id="myTable" class="table"> 
                 <thead class="bg-info">
                     <tr>
                         <th scope="col">ID</th>
@@ -71,9 +72,16 @@
         </div>
 
     </div>
-
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+</script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    
 </body>
 
 </html>
